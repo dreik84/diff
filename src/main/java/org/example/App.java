7be0a75c51e53db26ebfa439bcdf77f30cb38891
dreik84 <dreik84@mail.ru>
2025-09-26 16:27:33 +0300

    Create App.java, program entry point

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello diff");
+
+        for (String arg : args) {
+            System.out.println(arg);
+        }
+    }
+}
