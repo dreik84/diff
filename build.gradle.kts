24699bb0a3a34f1a975e3aa703d6ac520281c9d7
dreik84 <dreik84@mail.ru>
2025-09-29 15:43:29 +0300

    Update build.gradle.kts, add jackson-databind

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,6 +12,7 @@ repositories {
 }
 
 dependencies {
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.19.2")
     implementation("info.picocli:picocli:4.7.7")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
