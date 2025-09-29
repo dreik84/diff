7624b97f1043c133dcf32f06c4b6e86d861f8b14
dreik84 <dreik84@mail.ru>
2025-09-29 15:41:37 +0300

    Update App.java, add picocli

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,11 +1,34 @@
 package org.example;
 
-public class App {
-    public static void main(String[] args) {
-        System.out.println("Hello diff");
+import picocli.CommandLine;
 
-        for (String arg : args) {
-            System.out.println(arg);
-        }
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "1.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    @CommandLine.Parameters(index = "0", paramLabel = "filepath1", description = "path to first file")
+    private String filepath1;
+
+    @CommandLine.Parameters(index = "1", paramLabel = "filepath2", description = "path to second file")
+    private String filepath2;
+
+    @CommandLine.Option(names = {"-f", "--format"}, paramLabel = "format", description = "output format [default: stylish]")
+    private String format = "stylish";
+
+    public static void main(String[] args) {
+        int exitCode = new CommandLine(new App()).execute(args);
+        System.exit(exitCode);
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        System.out.println("FilePath 1: " + filepath1);
+        System.out.println("FilePath 2: " + filepath2);
+        System.out.println("Format: " + format);
+
+        Differ.generate(filepath1, filepath2);
+        return 0;
     }
 }
