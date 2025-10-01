e53cb7b388859d5d05da9391c8c992b5a2aafb7f
dreik84 <dreik84@mail.ru>
2025-10-01 17:01:13 +0300

    Create Comparator.java

---
+++ src/main/java/org/example/Comparator.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class Comparator {
+
+    public static void compareMaps(Map<String, Object> map1, Map<String, Object> map2) {
+        List<Entry> result = new ArrayList<>();
+    }
+}
