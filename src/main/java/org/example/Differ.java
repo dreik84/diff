91ca3a689b8da090c563fc73b408ec67fa4b5fff
dreik84 <dreik84@mail.ru>
2025-10-16 16:54:45 +0300

    Update Differ.java

--- src/main/java/org/example/Differ.java
+++ src/main/java/org/example/Differ.java
@@ -1,39 +1,22 @@
 package org.example;
 
+import org.example.formatter.StylishFormatter;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 
 public class Differ {
-    public static String generate(String pathToFile1, String pathToFile2) throws IOException {
+    public static String generate(String pathToFile1, String pathToFile2, String format) throws IOException {
         Map<String, Object> map1 = Parser.parseFile(pathToFile1);
         Map<String, Object> map2 = Parser.parseFile(pathToFile2);
+
+        List<Entry> diffList = Comparator.compareMaps(map1, map2);
+        String result = StylishFormatter.diffToString(diffList);
 
         System.out.println(map1);
         System.out.println(map2);
 
-        return diffToString(Comparator.compareMaps(map1, map2));
-    }
-
-    private static String diffToString(List<Entry> entries) {
-        StringBuilder sb = new StringBuilder("{\n");
-
-        for (Entry entry : entries) {
-
-            switch (entry.getStatus()) {
-                case SAVED ->
-                        sb.append("    ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
-                case ADDED ->
-                        sb.append("  + ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
-                case DELETED ->
-                        sb.append("  - ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
-                case UPDATED ->
-                        sb.append("  - ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n")
-                        .append("  + ").append(entry.getKey()).append(": ").append(entry.getNewValue()).append("\n");
-                default -> throw new IllegalStateException("Invalid status of Entry");
-            }
-        }
-
-        return sb.append("}").toString();
+        return result;
     }
 }
