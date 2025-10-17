55d5f8f784700b2b3307823f3a2d8e0deec5f77c
dreik84 <dreik84@mail.ru>
2025-10-17 16:55:52 +0300

    Create PlainFormatter.java

---
+++ src/main/java/org/example/formatters/PlainFormatter.java
@@ -0,0 +1,30 @@
+package org.example.formatters;
+
+import org.example.Entry;
+
+import java.util.List;
+
+public class PlainFormatter implements Formatter{
+    @Override
+    public String diffToString(List<Entry> entries) {
+        StringBuilder sb = new StringBuilder("{\n");
+
+        for (Entry entry : entries) {
+
+            switch (entry.getStatus()) {
+                case SAVED ->
+                        sb.append("    ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+                case ADDED ->
+                        sb.append("  + ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+                case REMOVED ->
+                        sb.append("  - ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n");
+                case UPDATED ->
+                        sb.append("  - ").append(entry.getKey()).append(": ").append(entry.getValue()).append("\n")
+                                .append("  + ").append(entry.getKey()).append(": ").append(entry.getNewValue()).append("\n");
+                default -> throw new IllegalStateException("Invalid status of Entry");
+            }
+        }
+
+        return sb.append("}").toString();
+    }
+}
