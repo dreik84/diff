779f9d0a2812a92c1391fb339f7e70dcc9b57ca9
dreik84 <dreik84@mail.ru>
2025-10-19 16:53:26 +0300

    Update Formatter.java, clean imports

--- src/main/java/org/example/formatters/Formatter.java
+++ src/main/java/org/example/formatters/Formatter.java
@@ -1,9 +1,10 @@
 package org.example.formatters;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.example.Entry;
 
 import java.util.List;
 
 public interface Formatter {
-    String diffToString(List<Entry> entries);
+    String diffToString(List<Entry> entries) throws JsonProcessingException;
 }
