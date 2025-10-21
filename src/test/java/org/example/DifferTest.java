f61e032727a32ceb9ab1c6b8fabd321aaed5c046
dreik84 <dreik84@mail.ru>
2025-10-21 16:19:04 +0300

    Update DifferTest.java

--- src/test/java/org/example/DifferTest.java
+++ src/test/java/org/example/DifferTest.java
@@ -1,7 +1,7 @@
 package org.example;
 
 import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -21,37 +21,21 @@ class DifferTest {
     }
 
     @ParameterizedTest
-    @ValueSource(strings = {"json", "yml"})
-    void testJsonYamlToStylish(String extension) throws IOException {
+    @CsvSource({
+            "json, stylish",
+            "yml, stylish",
+            "json, plain",
+            "yml, plain",
+            "json, json",
+            "yml, json"
+
+    })
+    void testDiff(String extension, String format) throws IOException {
         String filepath1 = getFixturePath("file1." + extension);
         String filepath2 = getFixturePath("file2." + extension);
 
-        String expected = getFixture(getFixturePath("stylish.txt"));
-        String actual = Differ.generate(filepath1, filepath2, "stylish");
-
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"json", "yml"})
-    void testJsonYamlToPlain(String extension) throws IOException {
-        String filepath1 = getFixturePath("file1." + extension);
-        String filepath2 = getFixturePath("file2." + extension);
-
-        String expected = getFixture(getFixturePath("plain.txt"));
-        String actual = Differ.generate(filepath1, filepath2, "plain");
-
-        assertEquals(expected, actual);
-    }
-
-    @ParameterizedTest
-    @ValueSource(strings = {"json", "yml"})
-    void testJsonYamlToJson(String extension) throws IOException {
-        String filepath1 = getFixturePath("file1." + extension);
-        String filepath2 = getFixturePath("file2." + extension);
-
-        String expected = getFixture(getFixturePath("json.txt"));
-        String actual = Differ.generate(filepath1, filepath2, "json");
+        String expected = getFixture(getFixturePath(format + ".txt"));
+        String actual = Differ.generate(filepath1, filepath2, format);
 
         assertEquals(expected, actual);
     }
